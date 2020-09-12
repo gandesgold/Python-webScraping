5e70197b9d4fac3c7cba8eb6333e0730c0c0b530
Gandes Goldestan <37586165+gandesgold@users.noreply.github.com>
2020-09-12 14:21:23 +0700

    update+add csv

--- webScraping.py
+++ webScraping.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from selenium import webdriver
 from BeautifulSoup import BeautifulSoup
 import pandas as pd
@@ -25,3 +26,31 @@ for a in soup.findAll('a', href=True, attrs={'class':'tableList js-dataTooltip'}
 
     df=pd.DataFrame({'Book Name':books, 'Ratings':ratings, 'Scores':scores, 'Votes':votes})
     df.to_csv('books.csv', index=False, encoding='utf-8')
+=======
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+data = []
+page = requests.get('https://scrapethissite.com/pages/simple/')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+country = soup.find_all('div', class_='col-md-4 country')
+
+for c in country:
+        country = c.find('h3', class_='country-name').text
+        capital = c.find('span', class_='country-capital').text
+        population = c.find('span', class_='country-population').text
+        area = c.find('span', class_='country-area').text
+
+        data.append({
+            'Country': country,
+            'Capital': capital,
+            'Population': population,
+            'Area': area
+        })
+
+df = pd.DataFrame(data)
+
+df.to_csv('country.csv', index=False, encoding="utf-8")
+>>>>>>> update file
